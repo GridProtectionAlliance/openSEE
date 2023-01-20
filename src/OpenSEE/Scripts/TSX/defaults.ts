7a6ee640219d3e1ca23632d236879db64f7c4177
Christoph Lackner <52460212+clackner-gpa@users.noreply.github.com>
2023-01-20 09:41:39 -0500

    Fixed Time units that got lost

--- src/OpenSEE/Scripts/TSX/defaults.ts
+++ src/OpenSEE/Scripts/TSX/defaults.ts
@@ -244,7 +244,8 @@ export const defaultSettings = {
     } as OpenSee.IUnitCollection,
     TimeUnit: {
         current: 5, options: [
-            { label: "auto", short: "auto", factor: 0 },
+            { label: "seconds", short: "s", factor: 0 },
+            { label: "minutes", short: "min", factor: 0 },
             { label: "milliseconds", short: "ms", factor: 0 },
             { label: "milliseconds since record start", short: "ms since event", factor: 0 },
             { label: "cycles since record start", short: "cycles", factor: 0 },
