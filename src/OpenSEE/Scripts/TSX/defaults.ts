8b9852b21351ed5a4f00cd245d0c84c6a002ebe3
Christoph Lackner <52460212+clackner-gpa@users.noreply.github.com>
2023-05-19 19:37:55 -0400

    Time settings reorder (#50)
    * Reorder 'TimeUnit' Settings alphabetically.
    ---------
    
    Co-authored-by: adambhagemeyer <adam.hagemeyer00@gmail.com>

--- src/OpenSEE/Scripts/TSX/defaults.ts
+++ src/OpenSEE/Scripts/TSX/defaults.ts
@@ -1,4 +1,4 @@
-﻿//******************************************************************************************************
+//******************************************************************************************************
 //  defaults.d.ts - Gbtc
 //
 //  Copyright © 2019, Grid Protection Alliance.  All Rights Reserved.
@@ -259,13 +259,13 @@ export const defaultSettings = {
 
     } as OpenSee.IUnitCollection<OpenSee.IUnitSetting>,
     TimeUnit: {
-        current: 5, options: [
-            { label: "seconds", short: "s", factor: 0 },
-            { label: "minutes", short: "min", factor: 0 },
+        current: 0, options: [
+            { label: "auto", short: "auto", factor: 0 },
+            { label: "cycles since record start", short: "cycles", factor: 0 },
             { label: "milliseconds", short: "ms", factor: 0 },
             { label: "milliseconds since record start", short: "ms since event", factor: 0 },
-            { label: "cycles since record start", short: "cycles", factor: 0 },
-            { label: "auto", short: "auto", factor: 0 }
+            { label: "minutes", short: "min", factor: 0 },
+            { label: "seconds", short: "s", factor: 0 }
         ],
         useAutoLimits: true
     },
