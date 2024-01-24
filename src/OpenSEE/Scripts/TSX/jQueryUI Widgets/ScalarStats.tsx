42653e6e11c26e8ef784124ebb4d89e010087de4
prestoncraw <prestoncrawford530@gmail.com>
2024-01-24 13:03:05 -0500

    Updated layout of Scalar Stats component

--- src/OpenSEE/Scripts/TSX/jQueryUI Widgets/ScalarStats.tsx
+++ src/OpenSEE/Scripts/TSX/jQueryUI Widgets/ScalarStats.tsx
@@ -19,34 +19,36 @@
 //  05/14/2018 - Billy Ernest
 //       Generated original version of source code.
 //
+//  12/07/2023 - Preston Crawford
+//       Switched table elements to a gpa-gemstone component
 //******************************************************************************************************
 
 import * as React from 'react';
-import { WidgetWindow } from './Common';
+import Table from '@gpa-gemstone/react-table'
 
 interface Iprops {
-    closeCallback: () => void,
     exportCallback: () => void,
-    eventId: number,
-    isOpen: boolean,
-    position: [number, number],
-    setPosition: (t: number, l: number) => void
+}
+
+interface EventData {
+    Stat: string,
+    Value: string
 }
 
 const ScalarStatsWidget = (props: Iprops) => {
-    const [stats, setStats] = React.useState<Array<JSX.Element>>([]);
+    const [stats, setStats] = React.useState<EventData[]>([]);
 
     React.useEffect(() => {
         let handle = getData();
 
-        return () => { if (handle != undefined && handle.abort != undefined) handle.abort();}
-    }, [props.eventId])
+        return () => { if (handle != undefined && handle.abort != undefined) handle.abort(); }
+    }, [props])
 
     function getData(): JQuery.jqXHR {
-            
+
         let handle = $.ajax({
             type: "GET",
-            url: `${homePath}api/OpenSEE/GetScalarStats?eventId=${props.eventId}`,
+            url: `${homePath}api/OpenSEE/GetScalarStats?eventId=${eventID}`,
             contentType: "application/json; charset=utf-8",
             dataType: 'json',
             cache: true,
@@ -54,26 +56,47 @@ const ScalarStatsWidget = (props: Iprops) => {
         });
 
         handle.done((d) => {
-            setStats(Object.keys(d).map(item =>
-                <tr style={{ display: 'table', tableLayout: 'fixed', width: '100%' }} key={item}>
-                    <td>{item}</td>
-                    <td>{d[item]}</td>
-                </tr>));
+            let t = []
+            Object.keys(d).forEach(stat => {
+                t.push({ Stat: stat, Value: d[stat] })
+            })
+            setStats(t);
         })
         return handle;
-        }
+    }
 
     return (
-        <WidgetWindow show={props.isOpen} close={props.closeCallback} maxHeight={400} width={500} position={props.position} setPosition={props.setPosition} >
-            <table className="table" style={{ fontSize: 'small', marginBottom: 0 }}>
-                    <thead style={{ display: 'table', tableLayout: 'fixed', width: 'calc(100% - 1em)' }}>
-                        <tr><th>Stat</th><th>Value&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button className='btn btn-primary' onClick={() => props.exportCallback()}>Export(csv)</button></th></tr>
-                    </thead>
-                    <tbody style={{ maxHeight: 310, overflowY: 'auto', display: 'block' }}>
-                        {stats}
-                    </tbody>
-                </table>
-        </WidgetWindow>
+        <>
+            <div className="d-flex flex-column h-100 w-100" style={{padding: '10px'}}>
+                <div className="table-responsive h-100" style={{ maxHeight: '100%', overflowY: 'auto'}}>
+                    <Table<EventData>
+                        cols={[
+                            {
+                                field: 'Stat',
+                                key: 'Stat',
+                                label: 'Stat',
+                                headerStyle: { width: 'calc(30% - 8.25em - 130px)' },
+                                rowStyle: { width: 'calc(30% - 8.25em - 130px)' },
+                            },
+                            {
+                                field: 'Value',
+                                key: 'Value',
+                                label: 'Value',
+                                headerStyle: { width: 'calc(60% - 8.25em)' },
+                                rowStyle: { width: 'calc(60% - 8.25em)' },
+                            },
+                        ]}
+                        tableClass="table table-hover w-100"
+                        data={stats}
+                        sortKey={""}
+                        ascending={true}
+                        onSort={() => { }}
+                        onClick={() => { }}
+                        selected={() => false}
+                    />
+                </div>
+            </div>
+        </>
     );
 }
 
