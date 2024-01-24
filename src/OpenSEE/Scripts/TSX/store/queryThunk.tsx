7ce6c0f36737f8f05f1e33777ab55fd685c0dde1
prestoncraw <prestoncrawford530@gmail.com>
2024-01-24 13:10:42 -0500

    Refactor queryParams to include more useful state

--- src/OpenSEE/Scripts/TSX/store/queryThunk.tsx
+++ src/OpenSEE/Scripts/TSX/store/queryThunk.tsx
@@ -19,102 +19,117 @@
 //  11/01/2020 - C. Lackner
 //       Generated original version of source code.
 //
+//  01/24/2024 - Preston Crawford
+//       Refactored to incorporate more relevant state into queryParams
+//
 //******************************************************************************************************
-import { createAsyncThunk} from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import { OpenSee } from '../global';
 import * as queryString from "query-string";
-import { AddPlot, RemovePlot, SetAnalytic, SetMouseMode, SetTimeLimit, SetZoomMode } from './dataSlice';
-import { SetdisplayAnalogs, SetdisplayCur, SetdisplayDigitals, SetdisplayTCE, SetdisplayVolt, SetNavigation, SetTab } from './settingSlice';
+import { AddPlot, SetTimeLimit} from './dataSlice';
+import { SelectEnabledPlots, SetSinglePlot } from './settingSlice';
+import { SetEventID, SelectEventID } from './eventInfoSlice'
+import { SelectAnalytics, UpdateAnalytic } from '../store/analyticSlice';
+import { SelectEventList, SetOverlappingEventList } from '../store/overlappingEventsSlice'
 
+import * as _ from 'lodash'
 
-// #region [ Thunks ]
 
 export const updatedURL = createAsyncThunk('Settings/newURL', (arg: { query: string, initial: boolean }, { getState, dispatch }) => {
+    let parsedQuery = queryString.parse(arg.query);
 
-    let query = queryString.parse(arg.query);
-    let oldState = getState() as OpenSee.IRootState;
+    if (parsedQuery) {
+        if (parsedQuery.plots)
+            parsedQuery.plots = JSON.parse(parsedQuery.plots)
+        if (parsedQuery.overlappingInfo)
+            parsedQuery.overlappingInfo = JSON.parse(parsedQuery.overlappingInfo)
+    }
 
+
+    const query: OpenSee.Query = parsedQuery
+    const oldState = getState() as OpenSee.IRootState;
+    const enabledPlots = SelectEnabledPlots(oldState);
+    const oldEventID = SelectEventID(oldState);
+    const oldAnalytics = SelectAnalytics(oldState);
+    const oldOverlappingList = SelectEventList(oldState);
+
+    const analyticQuery: OpenSee.IAnalyticStore = {
+        Harmonic: ToInt(query.Harmonic),
+        Trc: ToInt(query.Trc), 
+        LPFOrder: ToInt(query.LPFOrder),
+        HPFOrder: ToInt(query.HPFOrder),
+        FFTCycles: ToInt(query.FFTCycles),
+        FFTStartTime: ToFloat(query.FFTStartTime)
+    }
+
+    const isAnalyticsEqual = _.isEqual(oldAnalytics, analyticQuery)
+    const isOverlappingListEqual = _.isEqual(oldOverlappingList, parsedQuery.overlappingInfo)
+
+    const noPlots = (query?.plots?.length === 0 && enabledPlots?.length === 0) || query?.plots === undefined
+
+    //Set SinglePlot
+    if (ToBool(query?.singlePlot) && ToBool(query?.singlePlot) !== undefined)
+        dispatch(SetSinglePlot(ToBool(query.singlePlot)))
+
+    //Set EventID
+    if (ToInt(query?.eventID) && !isNaN(query?.eventID) && query?.eventID !== 0 && ToInt(query?.eventID) !== oldEventID)
+        dispatch(SetEventID(ToInt(query.eventID)))
+
+    //Set TimeLimit
+    if (ToInt(query.startTime) != undefined && ToInt(query.endTime) != undefined && (oldState.Data.startTime != ToInt(query.startTime) || (oldState.Data.endTime != ToInt(query.endTime))))
+        dispatch(SetTimeLimit({ start: ToInt(query.startTime), end: ToInt(query.endTime) }))
+
+    if (!isOverlappingListEqual && query?.overlappingInfo)
+        dispatch(SetOverlappingEventList(query.overlappingInfo))
+
+    //Analytic Query
+    if (!isAnalyticsEqual) 
+        dispatch(UpdateAnalytic({settings: queryStringToNums(analyticQuery)}))
     
-    // Start with Settings Query
-
-    //set Defaults
-    query.displayVolt = (query.displayVolt == undefined ? 'true' : query.displayVolt);
-    query.displayCur = (query.displayCur == undefined ? 'true' : query.displayCur);
-    query.displayTCE = (query.displayTCE == undefined ? 'false' : query.displayTCE);
-    query.displayDigitals = (query.displayDigitals == undefined ? 'false' : query.displayDigitals);
-    query.displayAnalogs = (query.displayAnalogs == undefined ? 'false' : query.displayAnalogs);
-    query.Tab = (query.Tab == undefined ? 'Info' : query.Tab);
-    query.Navigation = (query.Navigation == undefined ? 'system' : query.Navigation);
-
-    if (ToBool(query.displayVolt) != oldState.Settings.displayVolt || arg.initial) {
-        if (!ToBool(query.displayVolt))
-            dispatch(RemovePlot({ DataType: "Voltage", EventId: oldState.Data.eventID }))
-        else
-            dispatch(AddPlot({ DataType: "Voltage", EventId: oldState.Data.eventID, NoCompress: ToBool(query.dbgNocompress) }))
-        dispatch(SetdisplayVolt(ToBool(query.displayVolt)));
+    // On initial load, add default plots (Voltage and Current) if there is none provided via query
+    if (noPlots && arg.initial) {
+        dispatch(AddPlot({ key: { EventId: oldState.Data.eventID, DataType: "Voltage" } }));
+        dispatch(AddPlot({ key: { EventId: oldState.Data.eventID, DataType: "Current" } }));
     }
 
-    if (ToBool(query.displayCur) != oldState.Settings.displayCur || arg.initial) {
-        if (!ToBool(query.displayCur))
-            dispatch(RemovePlot({ DataType: "Current", EventId: oldState.Data.eventID }))
-        else
-            dispatch(AddPlot({ DataType: "Current", EventId: oldState.Data.eventID, NoCompress: ToBool(query.dbgNocompress) }))
-        dispatch(SetdisplayCur(ToBool(query.displayCur)));
-    }
-    if (ToBool(query.displayAnalogs) != oldState.Settings.displayAnalogs || arg.initial) {
-        if (!ToBool(query.displayAnalogs))
-            dispatch(RemovePlot({ DataType: 'Analogs', EventId: oldState.Data.eventID }))
-        else
-            dispatch(AddPlot({ DataType: "Analogs", EventId: oldState.Data.eventID }))
-        dispatch(SetdisplayAnalogs(ToBool(query.displayAnalogs)));
-    }
-    if (ToBool(query.displayDigitals) != oldState.Settings.displayDigitals || arg.initial) {
-        if (!ToBool(query.displayDigitals))
-            dispatch(RemovePlot({ DataType: 'Digitals', EventId: oldState.Data.eventID }))
-        else
-            dispatch(AddPlot({ DataType: "Digitals", EventId: oldState.Data.eventID }))
-        dispatch(SetdisplayDigitals(ToBool(query.displayDigitals)))
-    }
-    if (ToBool(query.displayTCE) != oldState.Settings.displayTCE || arg.initial) {
-        if (!ToBool(query.displayTCE))
-            dispatch(RemovePlot({ DataType: 'TripCoil', EventId: oldState.Data.eventID }))
-        else
-            dispatch(AddPlot({ DataType: "TripCoil", EventId: oldState.Data.eventID }))
-        dispatch(SetdisplayTCE(ToBool(query.displayTCE)));
-    }
-    if (query.Navigation != oldState.Settings.Navigation)
-        dispatch(SetNavigation(query.Navigation as OpenSee.EventNavigation))
-    if (query.Tab != oldState.Settings.Tab)
-        dispatch(SetTab(query.Tab as OpenSee.Tab))
-   
-    // Data Query
-    oldState = getState() as OpenSee.IRootState;
-            //(state: OpenSee.IRootState) => state.Data.startTime,
-            //(state: OpenSee.IRootState) => state.Data.endTime,
+    //TODO: come up with a way to handle traces in queryString CHristoph recommended a grid of some a sort
+    else if (query?.plots?.length > 0) {
+        query.plots.forEach(plot => {
+            const plotChange = query.plots.length !== enabledPlots.length
+            const oldPlot = enabledPlots.find(p => p.key.DataType === plot.key.DataType && p.key.EventId === plot.key.EventId)
+            const isYLimitsEqual = _.isEqual(plot?.yLimits, oldPlot?.yLimits)
 
-    query.mouseMode = (query.mouseMode == undefined ? 'zoom' : query.mouseMode);
-    query.zoomMode = (query.zoomMode == undefined ? 'x' : query.zoomMode);
+            if (plotChange && ToBool(query?.singlePlot) && ToBool(query?.singlePlot) !== undefined && plot.key.EventId === -1) {
+                const plots = query.plots.filter(p => p.key.EventId !== -1 && p.key.DataType === plot.key.DataType)
+                plots.forEach(p => dispatch(AddPlot({ key: { DataType: p.key.DataType, EventId: p.key.EventId }, yLimits: !isYLimitsEqual ? plot.yLimits : undefined, isZoomed: plot.isZoomed  })))
+                return;
+            }
 
-    if (query.mouseMode != oldState.Data.mouseMode)
-        dispatch(SetMouseMode(query.mouseMode as OpenSee.MouseMode))
-
-    if (query.zoomMode != oldState.Data.zoomMode)
-        dispatch(SetZoomMode(query.zoomMode as OpenSee.ZoomMode))
-
-    if (oldState.Settings.Tab == 'Analytic' &&
-        query.Analytic != undefined &&
-        query.Analytic != oldState.Data.Analytic)
-        dispatch(SetAnalytic(query.Analytic as OpenSee.Analytic));
-
-    if (!arg.initial) {
-        if (ToInt(query.startTime) != undefined && ToInt(query.endTime) != undefined &&
-            (oldState.Data.startTime != ToInt(query.startTime) || (oldState.Data.endTime != ToInt(query.endTime))))
-            dispatch(SetTimeLimit({ start: ToInt(query.startTime), end: ToInt(query.endTime) }))
+            if (plotChange && plot.key.EventId !== -1) 
+               dispatch(AddPlot({ key: plot.key, yLimits: !isYLimitsEqual ? plot.yLimits : undefined, isZoomed: plot.isZoomed }));
+            
+        })
     }
-       
-    // Compare Query - special case since the selected events depend on loading everything
-   
+
 })
+
+export function ToInt(arg) {
+    if (arg == undefined)
+        return undefined;
+    let val = parseInt(arg);
+    if (isNaN(val))
+        return undefined;
+    return val;
+}
+
+export function ToFloat(arg) {
+    if (arg == undefined)
+        return undefined;
+    let val = parseFloat(arg);
+    if (isNaN(val))
+        return undefined;
+    return val;
+}
 
 function ToBool(arg) {
     if (arg == undefined)
@@ -126,11 +141,18 @@ function ToBool(arg) {
     return undefined;
 }
 
-function ToInt(arg) {
-    if (arg == undefined)
+function queryStringToNums(arg: OpenSee.IAnalyticStore ) {
+    if (arg == undefined) 
         return undefined;
-    let val = parseInt(arg);
-    if (isNaN(val))
-        return undefined;
-    return val;
+ 
+    let query = {};
+    Object.keys(arg).forEach(key => {
+        const num = parseFloat(arg[key]);
+        if (!isNaN(num)) 
+            query[key] = num;
+        else 
+            query[key] = arg[key];
+    });
+
+    return query as OpenSee.IAnalyticStore;
 }
