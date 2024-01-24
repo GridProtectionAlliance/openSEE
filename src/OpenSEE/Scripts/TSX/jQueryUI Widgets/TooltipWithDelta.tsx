edeb9e1b2f956ed3b615137a19d69eb4750d6dbc
prestoncraw <prestoncrawford530@gmail.com>
2024-01-24 10:28:45 -0500

    OSEE-7 : Refactored layout

--- src/OpenSEE/Scripts/TSX/jQueryUI Widgets/TooltipWithDelta.tsx
+++ src/OpenSEE/Scripts/TSX/jQueryUI Widgets/TooltipWithDelta.tsx
@@ -19,70 +19,61 @@
 //  05/14/2018 - Billy Ernest
 //       Generated original version of source code.
 //
+//  01/24/2024 - Preston Crawford
+//       Refactored layout  
+//
 //******************************************************************************************************
 
 import * as React from 'react';
 import moment = require('moment');
-import { WidgetWindow } from './Common';
-import { useSelector } from 'react-redux';
-import { selectHover, selectDeltaHoverPoints } from '../store/dataSlice';
-import { selectColor } from '../store/settingSlice';
+import { selectDeltaHoverPoints } from '../store/dataSlice';
+import { SelectColor } from '../store/settingSlice';
+import { useAppSelector } from '../hooks';
+import HoverContext from '../Context/HoverContext'
 
+const ToolTipDeltaWidget = () => {
+    const hover = React.useContext(HoverContext);
+    const points = useAppSelector(selectDeltaHoverPoints(hover.hover));
+    const colors = useAppSelector(SelectColor);
 
-interface Iprops {
-    closeCallback: () => void,
-    isOpen: boolean,
-    position: [number, number],
-    setPosition: (t: number, l: number) => void
-}
-
-const ToolTipDeltaWidget = (props: Iprops) => {
-    const hover = useSelector(selectHover);
-    const points = useSelector(selectDeltaHoverPoints);
-    const colors = useSelector(selectColor);
-
-
-    let data: Array<JSX.Element> = (props.isOpen? points.map((p, i) => <tr key={i}>
+    let data: Array<JSX.Element> = (points.map((p, i) => <tr key={i}>
         <td className="dot" style={{ background: colors[p.Color], width: '12px' }}>&nbsp;&nbsp;&nbsp;</td>
         <td style={{ textAlign: 'left' }}><b>{p.Name}</b></td>
         <td style={{ textAlign: "right" }}><b>{(p.Value * (p.Unit.short == 'pu' || p.Unit.short == 'pu/s' ? 1.0 / p.BaseValue : p.Unit.factor)).toFixed(2)} ({p.Unit.short})</b></td>
         <td style={{ textAlign: "right" }}><b>{(p.PrevValue * (p.Unit.short == 'pu' || p.Unit.short == 'pu/s' ? 1.0 / p.BaseValue : p.Unit.factor)).toFixed(2)} ({p.Unit.short})</b></td>
         <td style={{ textAlign: "right" }}><b>{((p.Value - p.PrevValue) * (p.Unit.short == 'pu' || p.Unit.short == 'pu/s' ? 1.0 / p.BaseValue : p.Unit.factor)).toFixed(2)} ({p.Unit.short})</b></td>
-    </tr>) : [])
+    </tr>))
 
 
-    let firstDate = hover[0];
-    let secondDate = props.isOpen && points.length > 0? points[0].Time : NaN;
+    let firstDate = hover.hover[0];
+    let secondDate = points.length > 0 ? points[0].Time : NaN;
 
     return (
-        <WidgetWindow show={props.isOpen} close={props.closeCallback} maxHeight={600} width={538} position={props.position} setPosition={props.setPosition} >
-            <div style={{ textAlign: 'center' }} >
-                <table className="table" style={{ display: 'block', overflowY: 'scroll', maxHeight: '576px', overflowX: 'hidden', marginBottom: 0 }}>
-                    <thead>
-                        <tr><td style={{ width: 34 }}></td>
-                            <td style={{width: 120 }}></td>
+        <div className="d-flex" style={{ width: '100%', height: '100%', textAlign: 'center' }}>
+            <table className="table" style={{ overflowY: 'auto', height: '100%', overflowX: 'hidden', margin: '3%' }}>
+                <thead>
+                    <tr><td style={{ width: 34 }}></td>
+                        <td style={{ width: 120 }}></td>
+                        <td style={{ width: 120, paddingLeft: 5, paddingRight: 5, paddingTop: 0, paddingBottom: 5 }}>
+                            <b>{(!isNaN(firstDate) ? moment(firstDate).utc().format("HH:mm:ss.SSSSSS") : null)}</b>
+                        </td>
+                        {!isNaN(secondDate) ? <>
                             <td style={{ width: 120, paddingLeft: 5, paddingRight: 5, paddingTop: 0, paddingBottom: 5 }}>
-                                <b>{(!isNaN(firstDate) ? moment(firstDate).utc().format("HH:mm:ss.SSSSSS") : null)}</b>
+                                <b>{(moment(secondDate).utc().format("HH:mm:ss.SSSSSS"))}</b>
                             </td>
-                            {!isNaN(secondDate) ? <>
-                                <td style={{ width: 120, paddingLeft: 5, paddingRight: 5, paddingTop: 0, paddingBottom: 5 }}>
-                                    <b>{(moment(secondDate).utc().format("HH:mm:ss.SSSSSS"))}</b>
-                                </td>
-                                <td style={{ width: 120, paddingLeft: 5, paddingRight: 5, paddingTop: 0, paddingBottom: 5 }}>
-                                    <b>{(!isNaN(firstDate) ? ((secondDate - firstDate) / 1000).toFixed(9) + ' (s)' : '')}</b>
-                                </td> 
-                            </> : <td colSpan={2} style={{ width: 240, paddingLeft: 5, paddingRight: 5, paddingTop: 0, paddingBottom: 5 }} >
-                                    <b>Select a Point</b>
-                                </td>}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data}
-                    </tbody>
-                </table>
-            </div>
-        </WidgetWindow>
-        
+                            <td style={{ width: 120, paddingLeft: 5, paddingRight: 5, paddingTop: 0, paddingBottom: 5 }}>
+                                <b>{(!isNaN(firstDate) ? ((secondDate - firstDate) / 1000).toFixed(9) + ' (s)' : '')}</b>
+                            </td>
+                        </> : <td colSpan={2} style={{ width: 240, paddingLeft: 5, paddingRight: 5, paddingTop: 0, paddingBottom: 5 }} >
+                            <b>Select a Point</b>
+                        </td>}
+                    </tr>
+                </thead>
+                <tbody>
+                    {data}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
