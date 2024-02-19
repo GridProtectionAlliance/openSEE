794ce8fe25ecc7437958b68862b2ca8c8e962e5e
prestoncraw <prestoncrawford530@gmail.com>
2024-02-19 13:49:35 -0500

    Removed irrelevant information from about component

--- src/OpenSEE/Scripts/TSX/Components/About.tsx
+++ src/OpenSEE/Scripts/TSX/Components/About.tsx
@@ -29,62 +29,51 @@ interface Iprops {
 }
 
 const About = (props: Iprops) => {
-
     return (
         <div>
-       
-
-        <div className="modal fade show" style={{ display: (props.isOpen ? 'block' : 'none') }} role="dialog">
-            <div className="modal-dialog" style={{maxWidth: 1200}} role="document">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h3 className="modal-title">About openSEE -- System Event Explorer</h3>
+            <div className="modal fade show" style={{ display: (props.isOpen ? 'block' : 'none') }} role="dialog">
+                <div className="modal-dialog" style={{ maxWidth: 1200 }} role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h3 className="modal-title">About openSEE -- System Event Explorer</h3>
                             <button type="button" className="close" onClick={() => { props.closeCallback() }}>
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div className="modal-body" style={{textAlign: 'left', maxHeight: 800}}>
-                            
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body" style={{ textAlign: 'left', maxHeight: 800 }}>
+                            <p>Version 3.0</p>
 
-                        <p>Version 3.0</p>
+                            <p>openSEE is a browser-based waveform display and analytics tool that is used to view waveforms recorded by DFRs, Power Quality meters, relays and other substation devices that are stored in the openXDA database.
+                                The link in the URL window of openSEE can be embedded in emails so that recipients can quickly access the waveforms being studied.</p>
 
-                        <p>openSEE is a browser-based waveform display and analytics tool that is used to view waveforms recorded by DFRs, Power Quality meters, relays and other substation devices that are stored in the openXDA database.
-                        The link in the URL window of openSEE can be embedded in emails so that recipients can quickly access the waveforms being studied.</p>
+                            <p><b>General Navigation Features</b></p>
 
-                        <p><b>General Navigation Features</b></p>
+                            <p>The navigational context of openSEE is relative to the "waveform-of-focus" -- the waveform displayed in the top-most collection of charts that is displayed when openSEE is first opened --
+                                typically after clicking a link to drill down into a specific waveform in the Open PQ Dashboard.
+                                Tools in openSEE allow the user to dig deeper and understand more about this waveform-of-focus.
+                                Tools in openSEE also enable users to easily change the waveform-of-focus from the initially loaded -- moving forward or back sequentially in time.
+                            </p>
 
-                        <p>The navigational context of openSEE is relative to the "waveform-of-focus" -- the waveform displayed in the top-most collection of charts that is displayed when openSEE is first opened --
-                        typically after clicking a link to drill down into a specific waveform in the Open PQ Dashboard.
-                        Tools in openSEE allow the user to dig deeper and understand more about this waveform-of-focus.
-                        Tools in openSEE also enable users to easily change the waveform-of-focus from the initially loaded -- moving forward or back sequentially in time.
-                        </p>
-
-                        <ul>
-                            <li><u>Waveform View Check Boxes</u> - These check boxes are used to select the individual charts that are displayed for each waveform being displayed.  Options are voltages, currents, and digitals.</li>
-                            <li><u>Info, Compare and Analytics Tabs</u> - These tabs are used to show summary information about the waveform of focus, select from one of 20+ analytics to perform on the waveform-of-focus, or to
-	                        find additional waveforms to compare to the waveform-of-focus -- and might be interesting in the case of double-ended fault location.</li>
-                            <li><u>Data Tools Menu</u> - This menu allows the user to "pop up" tools to display additional information about the waveform-of-focus.</li>
-                            <li><u>Region Select Zooming</u> - The waveform initially loads with the the time-scale set to the full length of the waveform capture. With the mouse, the user can select a region of the waveform to zoom in and see more detail.</li>
-	                        <li><u>Reset Zoom</u> - After zooming in, this button can be used to set the time-scale back to the full length of the waveform.</li>
-                            <li><u>Forward and Back Navigation</u> - Using the collection of controls in the upper-right of the openSEE display, the user can select the basis for changing to a new waveform-of-focus.  A selection of "system" means that user can step forward or back
-	                        to next event in the openXDA base globally (for all DFRs, PQ Meters, etc.),
-	                        i.e., what happened immediately previously or next on the system relative to the current waveform-of-focus.  A selection of "asset" (or "line") limits this navigation to just events on this asset.
-	                        A selection of "meter" limits this navigation to just events recorded by this substation device.</li>
+                            <ul>
+                                <li><u>Region Select Zooming</u> - The waveform initially loads with the the time-scale set to the full length of the waveform capture. With the mouse, the user can select a region of the waveform to zoom in and see more detail.</li>
+                                <li><u>Forward and Back Navigation</u> - Using the collection of controls in the upper-right of the openSEE display, the user can select the basis for changing to a new waveform-of-focus.  A selection of "system" means that user can step forward or back
+                                    to next event in the openXDA base globally (for all DFRs, PQ Meters, etc.),
+                                    i.e., what happened immediately previously or next on the system relative to the current waveform-of-focus.  A selection of "asset" (or "line") limits this navigation to just events on this asset.
+                                    A selection of "meter" limits this navigation to just events recorded by this substation device.</li>
                                 <li><u>Chart Trace Section</u> - To the right of each chart, the user has the ability to turn on and off individual traces.  Tabs are provided to organize these selections by data type.</li>
-                        </ul>
+                            </ul>
 
-                        <p>
-                            The open-source code for openSEE can be found on GitHub as part of the openXDA Project.   See: <a href="https://github.com/GridProtectionAlliance/openXDA">https://github.com/GridProtectionAlliance/openXDA</a>
-                        </p>
+                            <p>
+                                The open-source code for openSEE can be found on GitHub. See: <a href="https://github.com/GridProtectionAlliance/openSEE">https://github.com/GridProtectionAlliance/openSEE</a>
+                            </p>
 
-
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" onClick={() => { props.closeCallback() }}>Close</button>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-danger" onClick={() => { props.closeCallback() }}>Close</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
     );
 
