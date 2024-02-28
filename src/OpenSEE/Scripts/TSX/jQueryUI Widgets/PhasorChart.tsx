c3406c0a3f2d6bd81dc0eab806aeba37575129c9
prestoncraw <prestoncrawford530@gmail.com>
2024-02-28 12:35:24 -0500

    Added Overflow Container to PolarChart

--- src/OpenSEE/Scripts/TSX/jQueryUI Widgets/PhasorChart.tsx
+++ src/OpenSEE/Scripts/TSX/jQueryUI Widgets/PhasorChart.tsx
@@ -105,50 +105,52 @@ const PhasorChartWidget = () => {
 
     return (
         <div className="d-flex flex-column" style={{ width: '100%', height: '100%', zIndex: 1001, padding: '10px' }}>
-            <div style={{ zIndex: 1001, height: '50%' }}>
-                <svg ref={svgRef} width="100%" height="100%">
-                    <circle cx={svgSize.width / 2} cy={svgSize.height / 2} r={radius / 2.167} stroke="lightgrey" strokeWidth="1" fill='white' fillOpacity="0" />
-                    <circle cx={svgSize.width / 2} cy={svgSize.height / 2} r={radius} stroke="lightgrey" strokeWidth="1" fill='white' fillOpacity="0" />
-                    <line x1="0" y1={svgSize.height / 2} x2={svgSize.width} y2={svgSize.height / 2} style={{ stroke: 'lightgrey', strokeWidth: 2 }} />
-                    <line x1={svgSize.width / 2} y1="0" x2={svgSize.width / 2} y2={svgSize.height} style={{ stroke: 'lightgrey', strokeWidth: 2 }} />
-                    {VVector.map((v, i) => <path key={i} d={drawVectorSVG(v, scaleV)} style={{ stroke: colors[v.Color], strokeWidth: 3 }} />)}
-                    {IVector.map((v, i) => <path key={i} d={drawVectorSVG(v, scaleI)} style={{ stroke: colors[v.Color], strokeWidth: 3 }} />)}
-                </svg>
-            </div>
-            <div style={{ height: '50%' }}>
-                <table className="table" style={{ width: '100%', height: '100%' }}>
-                    <thead>
-                        <tr><th></th><th></th><th></th>{AssetList.map((item, index) => <React.Fragment key={index}><th key={index} style={{ marginLeft: 15 }}>Mag{MagUnits}</th><th >Ang{PhaseUnits}</th></React.Fragment>)}</tr>
-                    </thead>
-                    <tbody style={{ overflowY: 'scroll', width: '100%', height: '100%' }}>
-                        <tr>
-                            {AssetList.map(item => <th rowSpan={rowSpan} style={{ writingMode: 'vertical-lr', transform: 'rotate(180deg)', whiteSpace: 'nowrap', textAlign: 'center' }}>{item}</th>)}
-                        </tr>
-                        {VVector?.length > 0 ?
-                            VVector.map((vv, index) => (
-                                <React.Fragment key={index}>
-                                    <tr>
-                                        <td className="dot" style={{ background: colors[vv.Color] }}><b>&nbsp;&nbsp;&nbsp;</b></td>
-                                        <td><b>{vv.Color}</b></td>
-                                        {createTable(vv, index)}
-                                    </tr>
-                                </React.Fragment>
-                            ))
-                            : null}
-                        {IVector?.length > 0 ?
-                            IVector.map((iv, index) => (
-                                <React.Fragment key={index}>
-                                    <tr>
-                                        <td className="dot" style={{ background: colors[iv.Color] }}><b>&nbsp;&nbsp;&nbsp;</b></td>
-                                        <td>{iv.Color}</td>
-                                        {createTable(iv, index)}
-                                    </tr>
-                                </React.Fragment>
-                            ))
-                            : null}
+            <div style={{ height: '100%', overflow: 'auto' }}>
+                <div style={{ zIndex: 1001, height: '40%' }}>
+                    <svg ref={svgRef} width="100%" height="100%">
+                        <circle cx={svgSize.width / 2} cy={svgSize.height / 2} r={radius / 2.167} stroke="lightgrey" strokeWidth="1" fill='white' fillOpacity="0" />
+                        <circle cx={svgSize.width / 2} cy={svgSize.height / 2} r={radius} stroke="lightgrey" strokeWidth="1" fill='white' fillOpacity="0" />
+                        <line x1="0" y1={svgSize.height / 2} x2={svgSize.width} y2={svgSize.height / 2} style={{ stroke: 'lightgrey', strokeWidth: 2 }} />
+                        <line x1={svgSize.width / 2} y1="0" x2={svgSize.width / 2} y2={svgSize.height} style={{ stroke: 'lightgrey', strokeWidth: 2 }} />
+                        {VVector.map((v, i) => <path key={i} d={drawVectorSVG(v, scaleV)} style={{ stroke: colors[v.Color], strokeWidth: 3 }} />)}
+                        {IVector.map((v, i) => <path key={i} d={drawVectorSVG(v, scaleI)} style={{ stroke: colors[v.Color], strokeWidth: 3 }} />)}
+                    </svg>
+                </div>
+                <div style={{ height: '60%' }}>
+                    <table className="table" style={{ width: '100%', height: '100%' }}>
+                        <thead>
+                            <tr><th></th><th></th><th></th>{AssetList.map((item, index) => <React.Fragment key={index}><th key={index} style={{ marginLeft: 15 }}>Mag{MagUnits}</th><th >Ang{PhaseUnits}</th></React.Fragment>)}</tr>
+                        </thead>
+                        <tbody style={{ overflowY: 'scroll', width: '100%', height: '100%' }}>
+                            <tr>
+                                {AssetList.map(item => <th rowSpan={rowSpan} style={{ writingMode: 'vertical-lr', transform: 'rotate(180deg)', whiteSpace: 'nowrap', textAlign: 'center' }}>{item}</th>)}
+                            </tr>
+                            {VVector?.length > 0 ?
+                                VVector.map((vv, index) => (
+                                    <React.Fragment key={index}>
+                                        <tr>
+                                            <td className="dot" style={{ background: colors[vv.Color] }}><b>&nbsp;&nbsp;&nbsp;</b></td>
+                                            <td><b>{vv.Color}</b></td>
+                                            {createTable(vv, index)}
+                                        </tr>
+                                    </React.Fragment>
+                                ))
+                                : null}
+                            {IVector?.length > 0 ?
+                                IVector.map((iv, index) => (
+                                    <React.Fragment key={index}>
+                                        <tr>
+                                            <td className="dot" style={{ background: colors[iv.Color] }}><b>&nbsp;&nbsp;&nbsp;</b></td>
+                                            <td>{iv.Color}</td>
+                                            {createTable(iv, index)}
+                                        </tr>
+                                    </React.Fragment>
+                                ))
+                                : null}
 
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     )
