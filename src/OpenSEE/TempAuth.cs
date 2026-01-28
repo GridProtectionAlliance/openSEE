c2bb405759539638095725bc24aa92802962b617
Gabriel Santos <gcsantos@gridprotectionalliance.org>
2026-01-28 08:56:27 -0500

    replace auth with tempauth

---
+++ src/OpenSEE/TempAuth.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Principal;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace OpenSEE.Security
+{
+    public class TestAuthHandlerOptions : AuthenticationSchemeOptions { }
+
+    public class TestAuthHandler : AuthenticationHandler<TestAuthHandlerOptions>
+    {
+        public const string AuthenticationScheme = "Test";
+
+        public TestAuthHandler( IOptionsMonitor<TestAuthHandlerOptions> options, ILoggerFactory logger, UrlEncoder encoder) : base(options, logger, encoder) { }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var identity = new GenericIdentity(AuthenticationScheme);
+            var principal = new GenericPrincipal(identity, ["Administrator"]);
+            var ticket = new AuthenticationTicket(principal, AuthenticationScheme);
+
+            var result = AuthenticateResult.Success(ticket);
+
+            return Task.FromResult(result);
+        }
+    }
+}
