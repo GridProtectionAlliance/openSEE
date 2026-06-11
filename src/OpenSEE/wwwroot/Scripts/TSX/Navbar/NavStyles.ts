e8e5c13c10e3051795f307b6b16905670124928b
prestoncraw <prestoncrawford530@gmail.com>
2026-06-11 10:55:48 -0400

    update navstyles

--- src/OpenSEE/wwwroot/Scripts/TSX/Navbar/NavStyles.ts
+++ src/OpenSEE/wwwroot/Scripts/TSX/Navbar/NavStyles.ts
@@ -1,14 +1,19 @@
 import * as React from 'react';
 
+const navIconSize = 46;
+
 export const navIconButtonStyle: React.CSSProperties = {
     alignItems: 'center',
     display: 'inline-flex',
-    height: 46,
-    justifyContent: 'center'
+    flex: `0 0 ${navIconSize}px`,
+    height: navIconSize,
+    justifyContent: 'center',
+    width: navIconSize
 };
 
 export const navIconDropdownStyle: React.CSSProperties = {
-    height: 46
+    height: navIconSize,
+    width: navIconSize * .75
 };
 
 export const navIconDropdownButtonClass = 'd-inline-flex align-items-center justify-content-center';
